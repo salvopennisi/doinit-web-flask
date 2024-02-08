55b37d0952f28d89eebf728e254da3c05ab42b7c
salvopennisi <57835576+salvopennisi@users.noreply.github.com>
2024-02-08 08:47:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask
+from flask_cors import CORS
+from match import *
 
 app = Flask(__name__)
+CORS(app)  # Abilita CORS per tutti gli endpoint
 
+
+user = {
+          "Nome": "Marco"
+        , "Cognome": "Verde"
+        , "_id": "8pv9l632qt"
+        , "img_profile_link": "https://example.com/marco_profile.jpg"
+        , "Citta": "Como"
+        , "location": { "longitude":9.149410145408947, "latitude":45.939475900000005}
+        ,"Bz_interests": ["Turismo", "E-commerce","Digital Marketing"]
+        , "Skills": ["Gestione progetti", "Social Media Marketing"]
+        , "Hobbies": ["Escursionismo", "Musica"]      
+    }
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+def index(userInfo=None):
+    group = get_score(user)
+    print(group)
+    return group
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
