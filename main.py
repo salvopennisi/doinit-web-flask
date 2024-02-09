d9622b558b5b3e07674e4bd36ca1fa6fa4732081
salvopennisi <57835576+salvopennisi@users.noreply.github.com>
2024-02-09 09:56:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask_cors import CORS
 from match import *
 
@@ -6,22 +7,12 @@ app = Flask(__name__)
 CORS(app)  # Abilita CORS per tutti gli endpoint
 
 
-user = {
-          "Nome": "Marco"
-        , "Cognome": "Verde"
-        , "_id": "8pv9l632qt"
-        , "img_profile_link": "https://example.com/marco_profile.jpg"
-        , "Citta": "Como"
-        , "location": { "longitude":9.149410145408947, "latitude":45.939475900000005}
-        ,"Bz_interests": ["Turismo", "E-commerce","Digital Marketing"]
-        , "Skills": ["Gestione progetti", "Social Media Marketing"]
-        , "Hobbies": ["Escursionismo", "Musica"]      
-    }
 
-@app.route('/')
-def index(userInfo=None):
-    group = get_score(user)
-    print(group)
+@app.route('/', methods=['POST'])
+def index():
+    res = request.json
+    print(res)
+    group = get_score(res)
     return group
 
 if __name__ == '__main__':
