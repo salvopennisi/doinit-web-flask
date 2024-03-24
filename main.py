06dda0f39b657f9b6e467284ece9f476f522f29e
salvopennisi <salvopennisi545@gmail.com>
2024-03-24 11:13:59 +0100

    fix

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-from flask import Flask
-from flask import request
+import logging
+from flask import Flask, request
 from flask_cors import CORS
 from match import get_matching
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)  # Set the logging level to DEBUG
+
 app = Flask(__name__)
-CORS(app)  # Abilita CORS per tutti gli endpoint
-
-
+CORS(app)  # Enable CORS for all endpoints
 
 @app.route('/', methods=['POST'])
 def index():
-    res = request.json
-    print(res)
-    group = get_matching(res)
-    return group
+    try:
+        res = request.json
+        logging.debug("Received request data: %s", res)
+        
+        group = get_matching(res)
+        return group
+    except Exception as e:
+        logging.exception("An error occurred while processing the request: %s", str(e))
+        return "An error occurred while processing the request", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    logging.basicConfig(level=logging.DEBUG)  # Set the logging level to DEBUG
+    app.run(debug=True, host='0.0.0.0', port=8080)
--- match.py
+++ match.py
@@ -1,10 +1,11 @@
-import dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
 from neo4j import GraphDatabase
 import pandas as pd 
 import numpy as np
 import json 
 
+load_dotenv(find_dotenv())
 class Neo4jConnection:
     def __init__(self, uri, auth):
         self._uri = uri
@@ -25,29 +26,33 @@ class Neo4jConnection:
             return result.data()
 
 def get_user_by_proximity(connection, user_id:str, user_position:str, limit:int=100, max_distance:int=1000 ):
-    latitude = user_position['latitude']
-    longitude = user_position['longitude']
+    
+    latitude = user_position['y']
+    longitude = user_position['x']
     
     query = f"""
          MATCH (n:User)
-        WHERE point.distance(n.location, point({{latitude: {latitude}, longitude: {longitude}}}))/1000 <= {max_distance}
-        AND  n._id <> '{user_id}'
-        WITH n, point.distance(n.location, point({{latitude: {latitude}, longitude: {longitude}}}))/1000 as straight_line_distance
+        WHERE point.distance(n.position, point({{y: {latitude}, x: {longitude}}}))/1000 <= {max_distance}
+        AND  n.id <> '{user_id}'
+        WITH n, point.distance(n.position, point({{y: {latitude}, x: {longitude}}}))/1000 as straight_line_distance
         RETURN n, straight_line_distance
         ORDER BY straight_line_distance
         {'LIMIT ' + str(limit) if limit is not None else ''}
         """
+    
     result = connection.run_query(query)
+    print(result)
     connection.close()
     data = [{ **item["n"], "straight_line_distance": item["straight_line_distance"] } for item in result]
     df = pd.DataFrame(data)
+    print('get_user_prox')
     return df
 
 def calculate_proximity_score(distance, max_score:int=25,coefficient_score:float=0.15,increment_distance:int=50 ):
     return  max_score * (1 - coefficient_score) ** (distance / increment_distance)
 
-def calculate_bz_interest_score(user_bz_interests, table_bz_insterests, max_score:int=25):
-    bz_interest = set(table_bz_insterests) & set(user_bz_interests)
+def calculate_bz_interest_score(user_businessInterests, table_bz_insterests, max_score:int=25):
+    bz_interest = set(table_bz_insterests) & set(user_businessInterests)
     if len(bz_interest) > 1:
         divisor = max_score * 0.25 # per dare piu  importanza a una singola combinazione
         dividend = (1 + np.exp(-1 * (len(bz_interest) - 1)))
@@ -66,27 +71,28 @@ def calculate_hobbies_score(user_hobbies, table_hobbies, max_score:int=25):
     return (max_score / len(sum_hobbies)) * len(merged_hobby)
 
 def calculate_total_score(df):
-    df['total_score'] = df['proximity_score']+df['bz_interests_score']+df['skills_score']+df['hobbies_score']
+    df['total_score'] = df['proximity_score']+df['businessInterests_score']+df['skills_score']+df['hobbies_score']
     return df
 
 
 
 def get_scores(connection, user_info, threshold_score=70):
+    print('get_scores')
     try:
-        score_result = get_user_by_proximity(connection, user_info["_id"], user_info["location"])
+        score_result = get_user_by_proximity(connection, user_info["id"], user_info["position"])
         score_result['proximity_score'] = score_result['straight_line_distance'].apply(calculate_proximity_score)
 
         # Utilizza una funzione lambda per passare due parametri
-        score_result['bz_interests_score'] = score_result.apply(
-            lambda row: calculate_bz_interest_score(row['Bz_interests'], user_info['Bz_interests']),
+        score_result['businessInterests_score'] = score_result.apply(
+            lambda row: calculate_bz_interest_score(row['businessInterests'], user_info['businessInterests']),
             axis=1
         )
         score_result['skills_score'] = score_result.apply(
-            lambda row: calculate_skills_score(row['Skills'], user_info['Skills']),
+            lambda row: calculate_skills_score(row['skills'], user_info['skills']),
             axis=1
         )
         score_result['hobbies_score'] = score_result.apply(
-            lambda row: calculate_hobbies_score(row['Hobbies'], user_info['Hobbies']),
+            lambda row: calculate_hobbies_score(row['hobbies'], user_info['hobbies']),
             axis=1
         )
 
@@ -101,9 +107,11 @@ def get_scores(connection, user_info, threshold_score=70):
 
 
 def get_matching(user_info):
-    dotenv.load_dotenv()
+    
     URI = os.getenv("NEO4J_URI")
     AUTH = {"user": os.getenv("NEO4J_USERNAME"), "password": os.getenv("NEO4J_PASSWORD")}
+    print(URI)
+    print(AUTH)
     connection = Neo4jConnection(URI, AUTH)
     connection.connect()
     data = get_scores(connection, user_info)
@@ -116,15 +124,36 @@ def get_matching(user_info):
 if __name__ == "__main__":
     
     user_info =  {
-          "Nome": "Marco"
-        , "Cognome": "Verde"
-        , "_id": "8pv9l632qt"
-        , "img_profile_link": "https://example.com/marco_profile.jpg"
-        , "Citta": "Como"
-        , "location": { "longitude":9.149410145408947, "latitude":45.939475900000005}
-        ,"Bz_interests": ["Turismo", "E-commerce","Digital Marketing"]
-        , "Skills": ["Gestione progetti", "Social Media Marketing"]
-        , "Hobbies": ["Escursionismo", "Musica"]      
-    }
+	"id": "xNgGxgvORo",
+	"firstName": "Salvatore",
+	"lastName": "Pennisi",
+	"city": "Roma",
+	"position": {
+		"srid": {
+			"low": 4326,
+			"high": 0
+		},
+		"x": 12.4829321,
+		"y": 41.8933203
+	},
+	"hobbies": [
+		"Cooking",
+		"Bodybuilding",
+		"Reading"
+	],
+	"skills": [
+		"Web Development",
+		"Software Engineering",
+		"Catering"
+	],
+	"businessInterests": [
+		"Social Media",
+		"Web Development",
+		"Software Development",
+		"Real Estate"
+	],
+	"imgProfileLocation": "https://prflimgs.s3.eu-central-1.amazonaws.com/xNgGxgvORo/1665330192772.jpeg",
+	"bio": "Gesu di Nazaret"
+}
     
     print(get_matching(user_info))
