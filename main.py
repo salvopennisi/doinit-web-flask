34f271c9f29e370ea71dfbfeeedbce733fad7676
salvopennisi <57835576+salvopennisi@users.noreply.github.com>
2024-04-27 18:11:19 +0200

    Add files via upload
    fix

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import logging
 from flask import Flask, request
 from flask_cors import CORS
-from match import get_matching
+from match_people import get_matching_people
+from match_groups import get_matching_groups
+from dotenv import load_dotenv
+import json 
+
+load_dotenv("./.env")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)  # Set the logging level to DEBUG
@@ -9,18 +14,28 @@ logging.basicConfig(level=logging.INFO)  # Set the logging level to DEBUG
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all endpoints
 
-@app.route('/', methods=['POST'])
-def index():
+
+@app.route("/", methods=["POST"])
+def mainPeople():
     try:
+        # print(request.json)
         res = request.json
-        logging.debug("Received request data: %s", res)
-        
-        group = get_matching(res)
-        return group
+        # logging.debug("Received request data: %s", res)
+        people = get_matching_people(res)
+        groups = get_matching_groups(res)
+        return {"people": json.loads(people), "groups": json.loads(groups)}
     except Exception as e:
-        logging.exception("An error occurred while processing the request: %s", str(e))
+        # logging.exception("An error occurred while processing the request: %s", str(e))
         return "An error occurred while processing the request", 500
 
+<<<<<<< HEAD
+
+
+if __name__ == "__main__":
+    # Set the logging level to DEBUG
+    app.run(debug=False, host="0.0.0.0", port=8080)
+=======
 if __name__ == '__main__':
  # Set the logging level to DEBUG
     app.run(debug=True, host='0.0.0.0')
+>>>>>>> b6322f7c48fa8bb08bd695258df32d99dd67513a
